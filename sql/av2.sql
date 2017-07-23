6d833499fb9e8ee09f6e65912d610c5d40f31e35
peoft <Paulo Filho>
2017-07-22 21:38:07 -0300

    Script do BD MySQL.
    Criação do banco de dados e tabelas;

--- sql/av2.sql
+++ sql/av2.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE AV1;
-
+CREATE DATABASE AV2;
 USE AV2;
 CREATE TABLE Pessoa (
 id INT NOT NULL AUTO_INCREMENT,
@@ -13,28 +12,68 @@ CREATE TABLE Motorista (
 pessoa_id INT NOT NULL,
 numeroCNH varchar(255) NOT NULL,
 FOREIGN KEY ( pessoa_id )
-	REFERENCES Pessoa(id)
+	REFERENCES Pessoa( id )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-PRIMARY KEY (numeroCNH));
+PRIMARY KEY ( numeroCNH ));
 
 CREATE TABLE Funcionario (
 pessoa_id INT NOT NULL,
 matricula varchar(255) NOT NULL,
 FOREIGN KEY ( pessoa_id )
-	REFERENCES Pessoa(id)
+	REFERENCES Pessoa( id )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-PRIMARY KEY (matricula));
+PRIMARY KEY ( matricula ));
+
+CREATE TABLE Acessorio (
+id INT NOT NULL AUTO_INCREMENT,
+descricao varchar(50) NOT NULL,
+PRIMARY KEY ( id ) );
+
+CREATE TABLE Fabricante (
+id INT NOT NULL AUTO_INCREMENT,
+nome varchar(50) NOT NULL,
+PRIMARY KEY ( id ) );
+
+CREATE TABLE ModeloCarro (
+id INT NOT NULL AUTO_INCREMENT,
+fabricante_id INT NOT NULL,
+descricao varchar(50) NOT NULL,
+categoria  enum ('HATCH_COMPACTO', 'HATCH_MEDIO', 'SEDAN_COMPACTO', 'SEDAN_MEDIO', 'SEDAN_GRANDE', 'MINIVAN', 'ESPORTIVO', 'UTILITARIO_COMERCIAL'),
+FOREIGN KEY ( fabricante_id )
+	REFERENCES Fabricante( id ),
+PRIMARY KEY ( id ) );
+
+CREATE TABLE Carro (
+id INT NOT NULL AUTO_INCREMENT,
+acessorio_id INT NOT NULL,
+modeloCarro_id INT NOT NULL,
+placa varchar(10) NOT NULL,
+chassi varchar(30) NOT NULL,
+cor varchar(50) NOT NULL,
+valorDiaria double NOT NULL,
+FOREIGN KEY ( acessorio_id )
+	REFERENCES Acessorio( id ),
+FOREIGN KEY ( modeloCarro_id )
+	REFERENCES ModeloCarro( id ),
+PRIMARY KEY ( id ) );
 
 CREATE TABLE Aluguel (
 id INT NOT NULL AUTO_INCREMENT,
 pessoa_id INT NOT NULL,
+carro_id INT NOT NULL,
 dataPedido date NOT NULL,
 dataEntrega date NOT NULL,
 dataDevolucao date,
 valorTotal double NOT NULL,
 FOREIGN KEY ( pessoa_id )
-	REFERENCES Pessoa(id),
+	REFERENCES Pessoa( id ),
+FOREIGN KEY ( carro_id )
+	REFERENCES Carro( id ),
 PRIMARY KEY ( id ) );
+
+
+
+
 
